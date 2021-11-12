e2dbba5a385ff3842e50b3b054a1fb7b12696bcc
Gabriel RF <github@gabrf.com>
2021-11-12 16:37:26 -0300

    Accepting media

--- bot.py
+++ bot.py
@@ -23,10 +23,34 @@ def convert_thread(channel_thread, group_thread):
         {'$set': {'thread_id': group_thread}}
     )
 
-def add_user_db(user_id):
+def get_priority(value):
+    cases = {
+        0: '⬜️',
+        1: '🟦',
+        2: '🟩',
+        3: '🟨',
+        4: '🟧',
+        5: '🟥',
+    }
+    return cases.get(value)
+
+def update_thread(user_id):
+    data = search_user(user_id)
+    msg = f'{get_priority(data["priority"])} <a href="tg://user?id={data["user_id"]}">{data["user_id"]}</a> 👤 {data["name"]}'
+    return bot.edit_message_text(chat_id=sac_channel, message_id=data['channel_thread'], text=msg, parse_mode='HTML')
+
+def add_user_db(message):
     return db.users.insert_one({
-        'user_id': user_id,
+        'user_id': message.from_user.id,
+        'name': f'{message.from_user.first_name} {message.from_user.last_name}',
+        'priority': 1,
     })
+
+def update_user_info(user_id, key, arg):
+     return db.users.update_one(
+        {'user_id': user_id},
+        {'$set': {key: arg}},
+    )
 
 def add_user_thread(user_id, thread_id):
     return db.users.update_one(
@@ -50,6 +74,21 @@ def add_message(user_id, private_id, group_id, message):
         'message': str(message),
     })
 
+@bot.message_handler(content_types=['document', 'audio', 'photo', 'animation', 'voice_note', 'audio', 'sticker'])
+def documents(message):
+    if is_team_member(message.from_user.id):
+        channel_thread = message.json['reply_to_message']['message_id']
+        user = search_thread(channel_thread)
+        if user:
+            msg = bot.copy_message(user['user_id'], sac_group, message.message_id)
+        else:
+            user = search_message('group_id', channel_thread)
+            msg = bot.copy_message(user['user_id'], sac_group, message.message_id)
+    else:
+        user = search_user(message.from_user.id)
+        bot.copy_message(sac_group, message.from_user.id, message.message_id, reply_to_message_id=user['thread_id'])
+
+
 @bot.message_handler(content_types=['pinned_message'])
 @bot.channel_post_handler(content_types=['pinned_message'])
 def on_pin(message):
@@ -58,11 +97,33 @@ def on_pin(message):
 @bot.message_handler(commands=['fim'])
 def unpin(message):
     bot.unpin_chat_message(sac_channel, message.json['reply_to_message']['forward_from_message_id'])
+    user_id = message.reply_to_message.json['entities'][0]['user']['id']
+    try:
+        update_user_info(user_id, 'priority', int(0))
+        update_thread(user_id)
+        bot.reply_to(message, 'Atendimento encerrado.')
+        bot.send_message(user_id, 'Atendimento encerrado. Envie /start para um novo atendimento.')
+    except:
+        bot.delete_message(message.chat.id, message.message_id)
+
+
+@bot.message_handler(commands=['p'])
+def set_priority(message):
+    if is_team_member(message.from_user.id):
+        if message.text != '/p':
+            channel_thread = message.json['reply_to_message']['message_id']
+            user = search_thread(channel_thread)
+            priority = message.text.replace('/p ', '')
+            update_user_info(user['user_id'], 'priority', int(priority))
+            msg = update_thread(user['user_id'])
+            bot.delete_message(message.chat.id, message.message_id)
+        else:
+            bot.reply_to(message, 'Envie <code>/p valor</code> para definir a prioridade.', parse_mode='HTML')
+
     
 @bot.message_handler(func=lambda m:True)
 def on_message(message):
-    #print(message)
-    if message.from_user.id == 777000 and '👤' in message.text and '📟' in message.text:
+    if message.from_user.id == 777000 and '👤' in message.text and '🟦' in message.text:
         channel_thread = search_thread(message.forward_from_message_id)['thread_id']
         group_thread = message.message_id
         convert_thread(channel_thread, group_thread)
@@ -70,9 +131,9 @@ def on_message(message):
     if message.from_user.id > 777000 and not is_team_member(message.from_user.id):
         user = search_user(message.from_user.id)
         if not user:
-            add_user_db(message.from_user.id)
-            #msg = bot.copy_message(sac_channel, message.from_user.id, message.message_id)
-            msg = bot.send_message(sac_channel, f'📟 <a href="tg://user?id={message.from_user.id}">{message.from_user.id}</a>\n👤 {message.from_user.first_name}', parse_mode='HTML')
+            add_user_db(message)
+            msg = bot.send_message(sac_channel,
+                f'{get_priority(1)} <a href="tg://user?id={message.from_user.id}">{message.from_user.id}</a> 👤 {message.from_user.first_name} {message.from_user.last_name}', parse_mode='HTML')
             add_user_thread(message.from_user.id, msg.message_id)
         else:
             if message.reply_to_message:
@@ -83,20 +144,30 @@ def on_message(message):
                 reply_id = user['thread_id']
                 channel_thread = user['channel_thread']
                 bot.pin_chat_message(sac_channel, channel_thread, disable_notification=True)
+                try:
+                    update_user_info(user['user_id'], 'priority', int(1))
+                    update_thread(user['user_id'])
+                except:
+                    pass
             msg = bot.send_message(sac_group, message.text, reply_to_message_id=reply_id)
         add_message(message.from_user.id, message.message_id, msg.message_id, message)
     if message.from_user.id > 777000 and is_team_member(message.from_user.id):
-        if search_thread(message.reply_to_message.message_id):
-            reply_id = search_thread(message.reply_to_message.message_id)
-            msg = bot.send_message(reply_id['user_id'], message.text)
-        else:
-            reply_id = search_message('group_id', message.reply_to_message.message_id)
-            msg = bot.send_message(reply_id['user_id'], message.text, reply_to_message_id=reply_id['private_id'])
-        add_message(reply_id['user_id'], msg.message_id, message.message_id, msg)
+        try:
+            if search_thread(message.reply_to_message.message_id):
+                reply_id = search_thread(message.reply_to_message.message_id)
+                msg = bot.send_message(reply_id['user_id'], message.text)
+            else:
+                reply_id = search_message('group_id', message.reply_to_message.message_id)
+                msg = bot.send_message(reply_id['user_id'], message.text, reply_to_message_id=reply_id['private_id'])
+            add_message(reply_id['user_id'], msg.message_id, message.message_id, msg)
+        except AttributeError:
+            bot.reply_to(message, 'Utilize os comentários do canal para responder as pessoas.')
 
 
 @bot.edited_message_handler(func=lambda m:True)
 def on_edit(message):
+    if message.from_user.id == 777000:
+        return
     if not is_team_member(message.from_user.id):
         group_id = search_message('private_id', message.json['message_id'])['group_id']
         bot.edit_message_text(message.text, sac_group, group_id)
@@ -104,10 +175,10 @@ def on_edit(message):
         chat_id = search_message('group_id', message.message_id)
         bot.edit_message_text(message.text, chat_id['user_id'], chat_id['private_id'])
 
-#@bot.chat_member_handler(func=lambda m:True)
-#def on_chat_action(message):
-    #if message.new_chat_member.status == 'member':
-        #bot.ban_chat_member(message.chat.id, message.new_chat_member.user.id)
-        #bot.unban_chat_member(message.chat.id, message.new_chat_member.user.id)
+@bot.chat_member_handler(func=lambda m:True)
+def on_chat_action(message):
+    if message.new_chat_member.status == 'member':
+        bot.ban_chat_member(message.chat.id, message.new_chat_member.user.id)
+        bot.unban_chat_member(message.chat.id, message.new_chat_member.user.id)
 
 bot.polling(allowed_updates=telebot.util.update_types)
